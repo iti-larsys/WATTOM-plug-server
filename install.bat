63ab82d403c86ae4da5601b4d2c2440f328b0980
perezjquim <jquim1@hotmail.com>
2018-06-01 01:26:15 +0100

    Windows batch files fixed

--- install.bat
+++ install.bat
@@ -1,2 +1,20 @@
-cd Node-Server-Plugs
-npm rebuild
+@echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@echo WATTAPP - INSTALLATION
+@echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+@echo Downloading Bonjour SDK...
+@certutil.exe -urlcache -split -f "https://softpedia-secure-download.com/dl/b9c690fc6368102d17055b23e177dd1d/5b108802/100159413/software/programming/bonjoursdksetup.exe" b.exe
+@echo Bonjour SDK downloaded!
+
+@echo Installing Bonjour SDK...
+@b.exe
+@echo Bonjour SDK installed!
+
+@cd %~dp0
+@cd Node-Server-Plugs
+
+@echo Rebuilding NPM...
+@npm rebuild
+@echo NPM rebuilt!
+
+@echo DONE!!
--- startup.bat
+++ startup.bat
@@ -1,7 +1,11 @@
-echo "»»»»»»»»»»»»»»»»»»««««««««««««««««««««" 
-echo "»»»»»»»»»»»»»» WATTAPP «««««««««««««««"
-@ipconfig/all | find "IPv4"
-echo "»»»»»»»»»»»»»»»»»»««««««««««««««««««««" 
-echo "»»»»»»»»»»»»»»»»»»««««««««««««««««««««" 
-cd Node-Server-Plugs
-npm start
+@echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@echo WATTAPP
+@ipconfig |findstr "IPv4" | cut -f2 -d ':' > tmp
+@set /p ip=<tmp
+@echo http://%ip:~1%:3000
+@del tmp
+@echo @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+
+@cd %~dp0
+@cd Node-Server-Plugs
+@npm start
