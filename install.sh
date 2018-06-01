1fed1ca389c124cac6d758afb0e8dd741145876c
Lipegno <filp3q@gmail.com>
2018-06-01 01:26:15 +0100

    Windows batch files fixed

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
+echo "\033[1;31m@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" 
+echo "WATTAPP - INSTALLATION"
+echo "\033[1;31m@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@" 
+
+echo "Installing packages..."
 sudo apt-get install libavahi-compat-libdnssd-dev
+echo "Packages installed!"
+
 cd Node-Server-Plugs
+
+echo "Rebuilding NPM..."
 npm rebuild
+echo "NPM rebuilt!"
+
+echo "DONE!"
 
